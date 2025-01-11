eedebd25ee541c9a8b9dcb9bef4cb71276fc199f
ALASHI1 <alashimuyiwa@gmail.com>
2025-01-11 16:27:54 +0100

    another

--- example.py
+++ example.py
@@ -1,8 +1,6 @@
-
-
 print("This is an example for a test")
 print("This is a second line.....")
-
 print("This is a third line.....")
 print("This is a fourth line.....")
 print("This is a fifth line.....")
+print("This is a sixth line.....")
